a8781adbe6a9958ee075dfdd23e7ae0b5b90e11f
aboutto <voalvu@gmail.com>
2021-03-28 15:53:10 +0300

    first commit

---
+++ Members.js
@@ -0,0 +1,19 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Dasha Coyer',
+    email: 'dasga@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Coyote Martens',
+    email: 'coyot@gmail.com'
+  },
+]
+
+module.exports = members
